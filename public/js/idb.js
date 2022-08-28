a33398986f1e835a7d07c43369fe049ccdecae35
goforward-z <zlepeng.ucf@gmail.com>
2022-08-28 17:42:10 -0400

    add ability to check if the app is online

--- public/js/idb.js
+++ public/js/idb.js
@@ -8,3 +8,17 @@ request.onupgradeneeded = function(event) {
     const db = event.target.result;
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
+
+// upon a successful 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store 
+    db = event.target.result;
+    // check if app is online
+    if (navigator.onLine) {
+      uploadTransaction();
+    }
+  };
+  
+request.onerror = function(event) {
+console.log(event.target.errorCode);
+};
