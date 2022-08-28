aa2d6e5d7db424f3558297bc81d8b551d3f25a01
goforward-z <zlepeng.ucf@gmail.com>
2022-08-28 17:23:20 -0400

    establish a connection to indexedDB database

---
+++ public/js/idb.js
@@ -0,0 +1,10 @@
+// create variable to hold db connection
+let db;
+// establish a connection to IndexedDB database 
+const request = indexedDB.open('budget_tracker', 1);
+
+// this event will emit if the database version changes
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
