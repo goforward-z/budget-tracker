1e705592ac3237c2a49525a5d1d84959cdeae265
goforward-z <zlepeng.ucf@gmail.com>
2022-08-28 17:16:15 -0400

    installing the service worker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -19,3 +19,13 @@ const FILES_TO_CACHE = [
   '/icons/icon-384x384.png',
   '/icons/icon-512x512.png'  
 ];
+
+// adding files to the precache so that the application can use the cache (installing service worker)
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+          console.log('installing cache : ' + CACHE_NAME)
+          return cache.addAll(FILES_TO_CACHE)
+        })
+      )
+  });
